c88e063dc9b49672063488d98cb2fe8cc72086ff
manisha2412 <manishasharma.155@gmail.com>
2018-08-10 11:24:56 +0530

    Spring Boot API and Swagger impl

---
+++ src/main/java/com/bhaiti/rest/controller/WelcomeController.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.bhaiti.rest.controller;
+
+/**
+ * @author Abhijit P
+ * This is a sample controller class to handle get request 
+ */
+import com.bhaiti.beans.Welcome;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class WelcomeController {
+
+	private static final String welcomemsg = "Welcome Mr. %s!";
+
+    @GetMapping("/welcome/user")
+    @ResponseBody
+    public Welcome welcomeUser(@RequestParam(name="name", required=false, defaultValue="Java Fan") String name) {
+       System.out.println("inside welcome controller");
+    	return new Welcome(String.format(welcomemsg, name));
+    }
+
+}
